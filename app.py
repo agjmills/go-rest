2c405710a95fb4dffe20a54cb108dcc8b328773e
Alex Mills <alex@asdfx.us>
2021-01-21 20:17:59 +0000

    initial attempt with data structures

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
-import sys
 import nmap
 
 def handler(event, context):
     host = event["host"]
     ports = ','.join([str(port) for port in event["ports"]])
+
+    return scan(host, ports)
+
+def scan(host, ports):
     scanner = nmap.PortScanner()
-    return scanner.scan(host, ports)
+    scanner.scan(host, ports)
+
+    hosts = []
+    for host in scanner.all_hosts():
+        newHost = Host(host, scanner[host].hostname(), scanner[host].state())
+
+        for proto in scanner[host].all_protocols():
+            lport = scanner[host][proto].keys()
+            lport.sort()
+            for port in lport:
+                newPort = Port(proto, port, scanner[host][proto][port]['state'])
+                newHost.addPort(newPort)
+        hosts.append(newHost)
+    return hosts
+
+class Host:
+    def __init__(self, host, hostname, state):
+        self.host = host
+        self.hostname = hostname
+        self.state = state
+        self.ports = []
+
+    def addPort(self, port):
+        self.ports.append(port)
+
+class Port:
+    def __init__(self, protocol, port, state):
+        self.protocol = protocol
+        self.port = port
+        self.state = state
